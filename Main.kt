602f93677aa3a5c726a6090ebe8bbcba389d1f4b
KodeJaROM <138987362+KodeJaROM@users.noreply.github.com>
2023-08-08 06:48:19 -0500

    Add stage 5 of 6

--- Main.kt
+++ Main.kt
@@ -23,52 +23,75 @@ fun main() {
         )
     }
 
-    printSupplies(water, milk, beans, cups, money)
-    println("Write action (buy, fill, take):")
-    val input = readln()
+    while (true) {
+        println("Write action (buy, fill, take, remaining, exit):")
+        val input = readln()
 
-    when (input) {
-        "buy" -> {
-            println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-            val cType = readln().toInt()
-            when (cType) {
-                1 -> {
-                    water -= 250
-                    beans -= 16
-                    money += 4
+        when (input) {
+            "exit" -> break
+            "buy" -> {
+                println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+                val cType = readln()
+                when (cType) {
+                    "back" -> continue
+                    "1" -> {
+                        if (water > 250) {
+                            water -= 250
+                            beans -= 16
+                            money += 4
+                        } else {
+                            println("Sorry, not enough water!")
+                            continue
+                        }
+                    }
+
+                    "2" -> {
+                        if (water > 350) {
+                            water -= 350
+                            milk -= 75
+                            beans -= 20
+                            money += 7
+                        } else {
+                            println("Sorry, not enough water!")
+                            continue
+                        }
+                    }
+
+                    "3" -> {
+                        if (water > 200) {
+                            water -= 200
+                            milk -= 100
+                            beans -= 12
+                            money += 6
+                        } else {
+                            println("Sorry, not enough water!")
+                            continue
+                        }
+                    }
                 }
-                2 -> {
-                    water -= 350
-                    milk -= 75
-                    beans -= 20
-                    money += 7
-                }
-                3 -> {
-                    water -= 200
-                    milk -= 100
-                    beans -= 12
-                    money += 6
-                }
+                cups -= 1
+                println("I have enough resources, making you a coffee!")
             }
-            cups -= 1
-            printSupplies(water, milk, beans, cups, money)
-        }
-        "fill" -> {
-            println("Write how many ml of water you want to add:")
-            water += readln().toInt()
-            println("Write how many ml of milk you want to add:")
-            milk += readln().toInt()
-            println("Write how many grams of coffee beans you want to add:")
-            beans += readln().toInt()
-            println("Write how many disposable cups you want to add:")
-            cups += readln().toInt()
 
-            printSupplies(water, milk, beans, cups, money)
-        }
-        "take" -> {
-            println("I gave you $$money")
-            money = 0
-            printSupplies(water, milk, beans, cups, money)
+            "fill" -> {
+                println("Write how many ml of water you want to add:")
+                water += readln().toInt()
+                println("Write how many ml of milk you want to add:")
+                milk += readln().toInt()
+                println("Write how many grams of coffee beans you want to add:")
+                beans += readln().toInt()
+                println("Write how many disposable cups you want to add:")
+                cups += readln().toInt()
+            }
+
+            "take" -> {
+                println("I gave you $$money")
+                money = 0
+            }
+
+            "remaining" -> {
+                printSupplies(water, milk, beans, cups, money)
+            }
         }
     }
 }
