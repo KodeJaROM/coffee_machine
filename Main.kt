bf84600f078dbcefd6684c364ae5a475f2b9d535
KodeJaROM <138987362+KodeJaROM@users.noreply.github.com>
2023-08-19 14:19:13 -0500

    Use functions

--- Main.kt
+++ Main.kt
@@ -23,77 +23,82 @@ fun main() {
         )
     }
 
+    fun fillCoffeeMachine() {
+        println("Write how many ml of water you want to add:")
+        water += readln().toInt()
+        println("Write how many ml of milk you want to add:")
+        milk += readln().toInt()
+        println("Write how many grams of coffee beans you want to add:")
+        beans += readln().toInt()
+        println("Write how many disposable cups you want to add:")
+        cups += readln().toInt()
+    }
+
+    fun takeMoney() {
+        println("I gave you $$money")
+        money = 0
+    }
+
+    data class CoffeeRecipe(val name: String, val water: Int, val milk: Int, val beans: Int, val cost: Int)
+
+    val coffeeRecipes = listOf(
+        CoffeeRecipe("Espresso", 250, 0, 16, 4),
+        CoffeeRecipe("Latte", 350, 75, 20, 7),
+        CoffeeRecipe("Cappuccino", 200, 100, 12, 6)
+    )
+
+    fun makeCoffee(recipe: CoffeeRecipe) {
+        val missingIngredient = when {
+            water < recipe.water -> "water"
+            milk < recipe.milk -> "milk"
+            beans < recipe.beans -> "coffee beans"
+            else -> null
+        }
+
+        if (missingIngredient == null) {
+            water -= recipe.water
+            milk -= recipe.milk
+            beans -= recipe.beans
+            money += recipe.cost
+            cups--
+            println("I have enough ingredients, making you a ${recipe.name}!")
+        } else {
+            println("Sorry, not enough $missingIngredient!")
+        }
+    }
+
+    fun buyCoffee() {
+        println("What do you want to buy?")
+        for ((index, recipe) in coffeeRecipes.withIndex()) {
+            println("${index + 1} - ${recipe.name} $${recipe.cost}")
+        }
+        val choice = readln()
+        if (choice == "back") {
+            return
+        } else {
+            val choiceInt = choice.toInt()
+            if (choiceInt in 1..coffeeRecipes.size) {
+                makeCoffee(coffeeRecipes[choiceInt - 1])
+            } else {
+                println("Invalid choice")
+            }
+        }
+    }
+
+
     while (true) {
         println("Write action (buy, fill, take, remaining, exit):")
         val input = readln()
 
         when (input) {
             "exit" -> break
-            "buy" -> {
-                println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-                val cType = readln()
-                when (cType) {
-                    "back" -> continue
-                    "1" -> {
-                        if (water > 250) {
-                            water -= 250
-                            beans -= 16
-                            money += 4
-                        } else {
-                            println("Sorry, not enough water!")
-                            continue
-                        }
-                    }
-
-                    "2" -> {
-                        if (water > 350) {
-                            water -= 350
-                            milk -= 75
-                            beans -= 20
-                            money += 7
-                        } else {
-                            println("Sorry, not enough water!")
-                            continue
-                        }
-                    }
-
-                    "3" -> {
-                        if (water > 200) {
-                            water -= 200
-                            milk -= 100
-                            beans -= 12
-                            money += 6
-                        } else {
-                            println("Sorry, not enough water!")
-                            continue
-                        }
-                    }
-                }
-                cups -= 1
-                println("I have enough resources, making you a coffee!")
-            }
-
-            "fill" -> {
-                println("Write how many ml of water you want to add:")
-                water += readln().toInt()
-                println("Write how many ml of milk you want to add:")
-                milk += readln().toInt()
-                println("Write how many grams of coffee beans you want to add:")
-                beans += readln().toInt()
-                println("Write how many disposable cups you want to add:")
-                cups += readln().toInt()
-            }
-
-            "take" -> {
-                println("I gave you $$money")
-                money = 0
-            }
-
-            "remaining" -> {
-                printSupplies(water, milk, beans, cups, money)
-            }
+            "buy" -> buyCoffee()
+            "fill" -> fillCoffeeMachine()
+            "take" -> takeMoney()
+            "remaining" -> printSupplies(water, milk, beans, cups, money)
         }
     }
 }
+
 
 
