b604e906ee65bd11b4b7554b511c78b7608ca642
KodeJaROM <138987362+KodeJaROM@users.noreply.github.com>
2023-08-28 06:35:52 -0500

    Refactor code

--- Main.kt
+++ Main.kt
@@ -1,15 +1,29 @@
 package machine
 
-fun main() {
+class CoffeeMachine {
+//    initial amounts of ingredients
+    private var water = 400
+    private var milk = 540
+    private var beans = 120
+    private var cups = 9
+    private var money = 550
 
-//    initial amounts of product available
-    var water = 400
-    var milk = 540
-    var beans = 120
-    var cups = 9
-    var money = 550
+    fun start() {
+        while (true) {
+            println("Write action (buy, fill, take, remaining, exit):")
+            val input = readln()
 
-    fun printSupplies(water: Int, milk: Int, beans: Int, cups: Int, money: Int) {
+            when (input) {
+                "exit" -> break
+                "buy" -> buyCoffee()
+                "fill" -> fillCoffeeMachine()
+                "take" -> takeMoney()
+                "remaining" -> printSupplies(water, milk, beans, cups, money)
+            }
+        }
+    }
+
+    private fun printSupplies(water: Int, milk: Int, beans: Int, cups: Int, money: Int) {
         println(
             """
         The coffee machine has:
@@ -23,7 +37,7 @@ fun main() {
         )
     }
 
-    fun fillCoffeeMachine() {
+    private fun fillCoffeeMachine() {
         println("Write how many ml of water you want to add:")
         water += readln().toInt()
         println("Write how many ml of milk you want to add:")
@@ -34,20 +48,20 @@ fun main() {
         cups += readln().toInt()
     }
 
-    fun takeMoney() {
+    private fun takeMoney() {
         println("I gave you $$money")
         money = 0
     }
 
     data class CoffeeRecipe(val name: String, val water: Int, val milk: Int, val beans: Int, val cost: Int)
 
-    val coffeeRecipes = listOf(
+    private val coffeeRecipes = listOf(
         CoffeeRecipe("Espresso", 250, 0, 16, 4),
         CoffeeRecipe("Latte", 350, 75, 20, 7),
         CoffeeRecipe("Cappuccino", 200, 100, 12, 6)
     )
 
-    fun makeCoffee(recipe: CoffeeRecipe) {
+    private fun makeCoffee(recipe: CoffeeRecipe) {
         val missingIngredient = when {
             water < recipe.water -> "water"
             milk < recipe.milk -> "milk"
@@ -67,7 +81,7 @@ fun main() {
         }
     }
 
-    fun buyCoffee() {
+    private fun buyCoffee() {
         println("What do you want to buy?")
         for ((index, recipe) in coffeeRecipes.withIndex()) {
             println("${index + 1} - ${recipe.name} $${recipe.cost}")
@@ -84,20 +98,11 @@ fun main() {
             }
         }
     }
+}
 
-
-    while (true) {
-        println("Write action (buy, fill, take, remaining, exit):")
-        val input = readln()
-
-        when (input) {
-            "exit" -> break
-            "buy" -> buyCoffee()
-            "fill" -> fillCoffeeMachine()
-            "take" -> takeMoney()
-            "remaining" -> printSupplies(water, milk, beans, cups, money)
-        }
-    }
+fun main() {
+    val coffeeMachine = CoffeeMachine()
+    coffeeMachine.start()
 }
 
 
